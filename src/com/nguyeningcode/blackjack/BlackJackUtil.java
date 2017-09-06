b58a77cdd8221bd50902018d4da6861b85b8bfef
howardjian <hcwjian@gmail.com>
2017-09-06 18:08:35 -0400

    added checks for bets and player balance

--- src/com/nguyeningcode/blackjack/BlackJackUtil.java
+++ src/com/nguyeningcode/blackjack/BlackJackUtil.java
@@ -31,51 +31,60 @@ public class BlackJackUtil {
         return numPlayers;
     }
 
-    public static void getBetsForPlayers(ArrayList<Player> players, double[] bets, double MIN_BUY_IN) {
+    public static void getBetsForPlayers(ArrayList<Player> players, double[] bets) {
         double tempBet = -1;
         boolean error;
         for (int i = 0; i < players.size(); i++) {
-            System.out.println("[" + players.get(i).getName() + "] Enter a bet for player \"" + players.get(i).getName() + "\" (>=" + MIN_BUY_IN + "): ");
+            System.out.println("[" + players.get(i).getName() + "] Enter a bet for player \"" + players.get(i).getName() + "\" (>=" + Game.MIN_BUY_IN + "): ");
             do {
                 try {
                     error = false;
                     tempBet = Double.parseDouble(in.nextLine());
                 } catch (Exception e) {
-                    System.out.println("[" + players.get(i).getName() + "] Please enter a valid number for your bet! (>=" + MIN_BUY_IN + "): ");
+                    System.out.println("[" + players.get(i).getName() + "] Please enter a valid number for your bet! (>=" + Game.MIN_BUY_IN + "): ");
                     error = true;
                 }
 
-                if (tempBet < MIN_BUY_IN && !error) {
-                    System.out.println("[" + players.get(i).getName() + "] Please enter a bet greater then the minimum buy in! (>=" + MIN_BUY_IN + "): ");
+                if (tempBet < Game.MIN_BUY_IN && !error) {
+                    System.out.println("[" + players.get(i).getName() + "] Please enter a bet greater then the minimum buy in! (>=" + Game.MIN_BUY_IN + "): ");
                 }
 
-            } while (tempBet < MIN_BUY_IN || error);
+                if(!checkBetAgainstBalance(players.get(i).balance, tempBet) && !error) {
+                    System.out.println("[" + players.get(i).getName() + "] You don't have enough to bet that! Your balance is " + players.get(i).balance + " but your bet was " + tempBet + "): ");
+                }
+
+            } while (tempBet < Game.MIN_BUY_IN || !checkBetAgainstBalance(players.get(i).balance, tempBet) || error);
             bets[i] = tempBet;
         }
     }
 
-    public static void getPlayers(ArrayList<Player> players, int numPlayers, double MIN_BUY_IN) {
+    public static void getPlayers(ArrayList<Player> players, int numPlayers) {
         boolean error;
         for (int i = 0; i < numPlayers; i++) {
             System.out.println("[Player #" + (i+1) + "] Please input your name: ");
             String name = in.nextLine();
             double balance = 0;
-            System.out.println("[" + name + "] Please input your balance (>=" + MIN_BUY_IN + "): ");
+            System.out.println("[" + name + "] Please input your balance (>=" + Game.MIN_BUY_IN + "): ");
             do {
                 try {
                     error = false;
                     balance = Double.parseDouble(in.nextLine());
                 } catch (Exception e) {
-                    System.out.println("[" + name + "] Please enter a valid number! (>=" + MIN_BUY_IN + "): ");
+                    System.out.println("[" + name + "] Please enter a valid number! (>=" + Game.MIN_BUY_IN + "): ");
                     error = true;
                 }
 
-                if (balance < MIN_BUY_IN && !error) {
-                    System.out.println("[" + name + "] Please enter a balance greater than " + MIN_BUY_IN + "! (>=" + MIN_BUY_IN + "): ");
+                if (balance < Game.MIN_BUY_IN && !error) {
+                    System.out.println("[" + name + "] Please enter a balance greater than " + Game.MIN_BUY_IN + "! (>=" + Game.MIN_BUY_IN + "): ");
                 }
-            } while (balance < MIN_BUY_IN || error);
+            } while (balance < Game.MIN_BUY_IN || error);
             players.add(new Player(name, balance));
         }
+    }
+
+
+    public static boolean checkBetAgainstBalance(double balance, double bet) {
+        return balance >= bet;
     }
 
 }
